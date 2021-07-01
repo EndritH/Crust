e59aa5f0276646a43d7b75849a48818e93dc4dc5
Endrit Hafizi <endrit.books@gmail.com>
2021-07-02 00:59:16 +0200

    seting SafeAreaView, // to do

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ padding: 16, backgroundColor: 'yellow'}}>
+          <Text>Search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: 'yellow'}}>
+          <Text>Search</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style='auto' />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
