25884e5249549a2560d23779d9b8353f21db1704
Endrit Hafizi <endrit.books@gmail.com>
2021-07-03 11:53:34 +0200

    setting the statusbar for android

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text, View, Platform } from 'react-native';
+import { StatusBar, StyleSheet, SafeAreaView, Text, View, Platform } from 'react-native';
 
+const isAndroid = Platform.OS === 'android';
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={{ padding: 16, backgroundColor: 'yellow'}}>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor: 'green'}}>
           <Text>Search</Text>
         </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: 'yellow'}}>
-          <Text>Search</Text>
+        <View style={{ flex: 1, padding: 16, backgroundColor: 'blue'}}>
+          <Text>List</Text>
         </View>
       </SafeAreaView>
-      <StatusBar style='auto' />
+      <ExpoStatusBar style='auto' />
     </>
   );
 }
